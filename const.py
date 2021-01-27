cfcddd0572cbd0dfbbc2102eb1f75e8b3c476ca2
adm.eremichev.i.a <diablero797@gmail.com>
2021-01-27 12:12:58 +0300

    разделил на две части: main и const

---
+++ const.py
@@ -0,0 +1,5 @@
+TOKEN = '1469485782:AAGri4fgnwXkWrt5sVp0HnYoKh3C-rtx6Zw'
+URL = 'https://api.telegram.org/bot{TOKEN}/{method}'
+
+UPDATE_METH = 'getUpdates'
+SEND_METH = 'sendMessage'
--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import requests
+import json
+import time
 
-TOKEN = '1469485782:AAGri4fgnwXkWrt5sVp0HnYoKh3C-rtx6Zw'
-URL = 'https://api.telegram.org/bot{TOKEN}/{method}'
-updates = 'getUpdates'
-send = 'sendMessage'
+import const
 
-data = {
-    'chat_id': '505372163',
-    'text': 'Hello from Test_bot'
-}
-url = URL.format(TOKEN=TOKEN, method=send)
-response = requests.post(url, data=data)
 
-print(response.text)
-
+def main():
+    while True:
+        url = const.URL.format(token=const.TOKEN, method=const.UPDATE_METH)
+        response = requests.get(url)
+        print(response)
+        time.sleep(2)
