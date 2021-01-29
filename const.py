3dbaac0804d14bf6c7f1f1434f73bcda073535a4
adm.eremichev.i.a <diablero797@gmail.com>
2021-01-29 16:44:22 +0300

    api add

--- const.py
+++ const.py
@@ -3,3 +3,14 @@ URL = 'https://api.telegram.org/bot{token}/{method}'
 
 UPDATE_METH = 'getUpdates'
 SEND_METH = 'sendMessage'
+
+MY_ID = 505372163
+
+UPDATE_ID_FILE_PATH = 'update_id'
+
+with open(UPDATE_ID_FILE_PATH) as file:
+    UPDATE_ID = file.readline()
+
+CITY = 'Tula'
+WEATHER_TOKEN = 'c20ea049a76d07a8d39defb5b25de0a3'
+WEATHER_URL = f'api.openweathermap.org/data/2.5/weather?q={CITY}&appid={WEATHER_TOKEN}'
--- main.py
+++ main.py
@@ -1,15 +1,50 @@
 import requests
 import json
 import time
-
+from pprint import pprint
 import const
+
+
+def get_message(data):
+    text = data['message']['text']
+    #return text
+    #print(text)
+
+
+def save_update_id(update):
+    #pprint(update)
+    with open(const.UPDATE_ID_FILE_PATH, 'w') as file:
+        file.write(str(update['update_id']))
+    return True
 
 
 def main():
     while True:
         url = const.URL.format(token=const.TOKEN, method=const.UPDATE_METH)
-        response = requests.get(url)
-        print(response)
+        content = requests.get(url).text
+
+        data = json.loads(content)
+        result = data['result'][::-1]
+        needed_part = None
+
+        for elem in result:
+            if elem['message']['chat']['id'] == const.MY_ID:
+                needed_part = elem
+                #print(needed_part['update_id'])
+                break
+
+        if const.UPDATE_ID != needed_part['update_id']:
+            print(const.UPDATE_ID)
+            print(needed_part['update_id'])
+
+
+            get_message(needed_part)
+            save_update_id(needed_part)
+
+        #pprint(needed_part)
+
+        # pprint(data)
+        break
         time.sleep(2)
 
 
